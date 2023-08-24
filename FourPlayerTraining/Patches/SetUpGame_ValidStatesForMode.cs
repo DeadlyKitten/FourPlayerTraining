98885d7812479218c094fbe999bcea43b9750938
DeadlyKitten <stevencarr777@gmail.com>
2023-08-24 16:12:30 -0700

    Fix CPU not being selectable for players 3 and 4.

---
+++ FourPlayerTraining/Patches/SetUpGame_ValidStatesForMode.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Nick;
+
+namespace FourPlayerTraining.Patches
+{
+    [HarmonyPatch(typeof(SetUpGame), "ValidStatesForMode")]
+    internal class SetUpGame_ValidStatesForMode
+    {
+        static void Postfix(CharacterSelectScreen.Mode mode, ref PlayerSlotContainer.State[] __result)
+        {
+            if (mode == CharacterSelectScreen.Mode.Training)
+            {
+                __result = new PlayerSlotContainer.State[3]
+                {
+                    PlayerSlotContainer.State.NoneSlot,
+                    PlayerSlotContainer.State.PlayerSlot,
+                    PlayerSlotContainer.State.CPUSlot
+                };
+            }
+        }
+    }
+}

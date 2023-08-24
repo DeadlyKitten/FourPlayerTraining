db16280993d00e330d230d57e7f629efa266d333
DeadlyKitten <stevencarr777@gmail.com>
2023-08-24 16:12:09 -0700

    Fix Training position reset to include players 3 and 4.

---
+++ FourPlayerTraining/Patches/TrainingUI_ClickedResetPos.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Nick;
+using System.Collections.Generic;
+
+namespace FourPlayerTraining.Patches
+{
+    [HarmonyPatch(typeof(TrainingUI), "ClickedResetPos")]
+    internal class TrainingUI_ClickedResetPos
+    {
+        private bool Prefix(ref GameInstance ___context)
+        {
+            var agents = new List<(int index,GameAgent agent)>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (___context.GetAgentFromPlayerIndex(i, out var agent))
+                {
+                    agents.Add((i,agent));
+                }
+            }
+
+            foreach (var (index, agent) in agents)
+            {
+                var spawnPoint = ___context.stage.GetFFASpawnPoint(agents.Count, index);
+                if(agent.TryGetMovement(out var movement))
+                {
+                    movement.TeleportTo(spawnPoint);
+                }
+            }
+
+            return false;
+        }
+    }
+}
